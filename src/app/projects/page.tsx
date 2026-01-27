3caa390d495089e9e336bab80694e2e0f9db4a35
Ramon Dias <ramondiasmonteiro@gmail.com>
2026-01-27 09:59:23 -0300

    :beetle: fix: Exclui a pasta de Projects

--- src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { ProjectCardsCarousel } from "./project-list"
-
-// export default function Page() {
-//   return (
-//     <div className="w-full xl:w-[85%] mx-auto">
-//       <ProjectCardsCarousel />
-//     </div>
-//   )
-// }
